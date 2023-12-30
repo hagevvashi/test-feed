1e0e48d233a06661bc9f7ca209deec804324693d
hagevvashi <hagevvashi@gmail.com>
2023-12-30 22:45:40 +0900

    chore: Enhance Docker Entrypoint with -f option for groupadd
    Enhance the Docker Entrypoint by introducing the -f option to the groupadd command. This modification prevents abnormal termination when the specified group already exists, resolving permission issues observed during file operations in Docker containers using bind mounts in development.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-groupadd -o -g ${GID} ${GNAME}
+groupadd -o -f -g ${GID} ${GNAME}
 useradd -o -l -u ${UID} -g ${GNAME} -m ${UNAME}
 chown ${UNAME}:${GNAME} /home/${UNAME}
 
