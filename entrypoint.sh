a2ac1e220f62bb7491976deb02c82a39c97cb11a
hagevvashi <hagevvashi@gmail.com>
2023-12-30 23:11:44 +0900

    chore: Reset group in Docker Entrypoint
    Implement group deletion using groupdel before adding it again with the groupadd command in the Docker Entrypoint. This modification resolves permission errors caused by inconsistencies in user and group relationships when executing file operations within Docker containers using bind mounts in development.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-groupadd -o -f -g ${GID} ${GNAME}
+groupdel ${GNAME}
+groupadd -o -g ${GID} ${GNAME}
 useradd -o -l -u ${UID} -g ${GNAME} -m ${UNAME}
 chown ${UNAME}:${GNAME} /home/${UNAME}
 
